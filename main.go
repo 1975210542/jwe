1a013bdb8298c8e64f4291e8ab5a92261b275058
高乾坤 <1975210542@>
2017-07-13 15:24:13 +0800

    1111

--- main.go
+++ main.go
@@ -17,30 +17,33 @@ import ( //	"crypto/rand"
 .2ypqAprYGAaSf7T-pHRfUg
 .ODE2OTVmNjZhZmU1OTA4NWI2MDc5MDgzNTkxNTZiNmE1YmFhNTZmYjljNmUyYmUwNmE4ZTJiNTFlZWU3ZGIzZQ*/
 func main() {
-	var args []string
+	args := make([]string, 0)
 	jwe := Jwe{}
-	//1
+	//1 生成头部
 	header := NewHeader(ALG_RSA1_5, ENC_A128CBC_HS256)
+	jsonHeader, err := JsonEncode(header)
+	args = append(args, jsonHeader)
+	fmt.Println(jsonHeader, err)
 
-	jHeader, _ := JsonEncode(header)
-	args = append(args, jHeader)
-	fmt.Println("jHeader:", jHeader)
-	//2
-	key, RsaKey := jwe.GetEncryptedKey(header, 16)
-	args = append(args, RsaKey)
-	fmt.Println(RsaKey)
+	//2 加密密钥
+	generateKey(1024)
+	key, RasKey := jwe.GetEncryptedKey(header, 16, []byte{})
+	args = append(args, string(RasKey))
+	fmt.Println("key:", string(key))
+	fmt.Println("RsaKey:", string(RasKey))
+
 	//3 4
 	plant := "gaoqiankun"
-	cipher, Iv := jwe.GetCipherText(header, key, plant)
-	fmt.Println("Cipher!!!!:", cipher)
-	args = append(args, string(cipher))
+	cipher, Iv := jwe.GetCipherText(header, []byte(plant), key)
 	args = append(args, string(Iv))
-	fmt.Println("len:", len(args))
-	//5
-	Atag := jwe.GetAuthenticationTag(header, []string{args[1], args[2], args[3]})
-	fmt.Println("Atag:", Atag)
-	args = append(args, Atag)
-	//6
+	args = append(args, string(cipher))
+
+	fmt.Println("cipher  Iv:", string(cipher), string(Iv))
+	//5 得到数字证书
+	Atag := jwe.GetAuthenticationTag(header, []string{string(RasKey), string(Iv), string(cipher)}, []byte{})
+	fmt.Println("Atag:", string(Atag))
+	args = append(args, string(Atag))
+	//6 得到jwe
 	jw := jwe.GetJWE(args)
 	fmt.Println("jwe:", jw)
 }
